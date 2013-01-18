40358ecfce27f2fce4dce8101535e717f3b030e2
Khanh Le <khanhlh@joomsolutions.com>
2013-01-18 15:28:02 +0700

    Megamenu Admin: update the toggle screen button

--- plugins/system/jat3v3/admin/megamenu/megamenu.tpl.php
+++ plugins/system/jat3v3/admin/megamenu/megamenu.tpl.php
@@ -172,8 +172,9 @@ $modules = $db->loadObjectList();
       </div> 
       
       <div class="toolbox-actions-group">
+        <button class="toolbox-action toolbox-togglescreen" data-action="toggleScreen" data-iconfull="icon-resize-full" data-iconsmall="icon-resize-small"><i class="icon-resize-full"></i></button>
+
         <button class="btn btn-success toolbox-action toolbox-saveConfig hide" data-action="saveConfig"><i class="icon-save"></i><?php echo JTexT::_('T3V3_NAVIGATION_MM_SAVE') ?></button>
-        <button class="btn toolbox-action toolbox-togglescreen" data-action="toggleScreen" data-screen="default"><i class="icon-resize-full"></i></button>
         <!--button class="btn btn-danger toolbox-action toolbox-resetConfig"><i class="icon-undo"></i><?php echo JTexT::_('T3V3_NAVIGATION_MM_RESET') ?></button-->
       </div>
 
