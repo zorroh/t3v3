f22fa6f961e2625099f225e313bbba03d714f9b2
zsinryu <zsinryu@live.com>
2013-01-19 18:11:52 +0700

    Fix Color 'inherit' on ThemeMagic

--- plugins/system/jat3v3/admin/thememagic/js/thememagic.js
+++ plugins/system/jat3v3/admin/thememagic/js/thememagic.js
@@ -350,14 +350,14 @@ var T3V3Theme = window.T3V3Theme || {};
 						adjust = null,
 						filter = this.className.match(/t3tm-(\w*)\s?/);
 
-					if(filter && $.isFunction(T3V3Theme['filter' + filter[1]])){
-						adjust = T3V3Theme['filter' + filter[1]](val);
-					}
+					//if(filter && $.isFunction(T3V3Theme['filter' + filter[1]])){
+					//	adjust = T3V3Theme['filter' + filter[1]](val);
+					//}
 
-					if(adjust != null && adjust != val){
-						val = adjust;
-						T3V3Theme.setValues(this, $.makeArray(val));
-					}
+					//if(adjust != null && adjust != val){
+					//	val = adjust;
+					//	T3V3Theme.setValues(this, $.makeArray(val));
+					//}
 
 					json[name] = val;
 				}
@@ -373,7 +373,7 @@ var T3V3Theme = window.T3V3Theme || {};
 		},
 
 		filtercolor: function(hex){
-			if(hex.charAt(0) === '@' || T3V3Theme.colors[hex.toLowerCase()]){
+			if(hex.charAt(0) === '@' || hex.toLowerCase() == 'inherit' || T3V3Theme.colors[hex.toLowerCase()]){
 				return hex;
 			}
 
